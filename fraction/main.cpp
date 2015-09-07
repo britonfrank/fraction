40971d636421d67ea8dfdb0d6a6907ee812d7e08
Briton Frank <britonfrank@gmail.com>
2015-09-07 08:06:34 -0700

    Initial Commit

---
+++ fraction/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  fraction
+//
+//  Created by Briton Frank on 9/7/15.
+//  Copyright © 2015 Briton Frank. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
